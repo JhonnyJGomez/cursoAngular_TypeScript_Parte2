6752f18d4e45c3df6670ef7ae0a42ac0faa86e22
Jhonny Gomez <jg.jhonny90@gmail.com>
2020-04-07 09:44:30 -0500

    Agrega directiva ngFor

--- holaMundo/src/app/directiva/directiva.component.ts
+++ holaMundo/src/app/directiva/directiva.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+interface producto{
+  nombre:string,
+  stock:number,
+  fabricante:string,
+  fechaVence:Date
+}
+
 @Component({
   selector: 'app-directiva',
   templateUrl: './directiva.component.html',
@@ -8,6 +15,26 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivaComponent implements OnInit {
   cargando:boolean= true
   
+  productos: Array<producto> = [{
+    nombre: 'Gaseosa',
+    stock: 15,
+    fabricante: 'Lindley',
+    fechaVence: new Date('12/24/2020')
+   },
+   {
+     nombre: 'arroz',
+     stock: 150,
+     fabricante: 'Arroz cauca',
+     fechaVence: new Date('06/24/2020')
+   },
+   {
+     nombre: 'salchicha',
+     stock: 215,
+     fabricante: 'Rancheras',
+     fechaVence: new Date('08/24/2020')
+   }]
+
+
   constructor() { }
 
 
@@ -16,6 +43,8 @@ export class DirectivaComponent implements OnInit {
     setTimeout (() => {
       this.cargando = false
    }, 3000);
+
+
   }
 
   alternar(){
