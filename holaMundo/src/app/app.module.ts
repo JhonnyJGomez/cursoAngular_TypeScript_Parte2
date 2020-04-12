64ac85dd5c3fccaa0d5ffcaf72fe4fbb5af893d6
Jhonny Gomez <jg.jhonny90@gmail.com>
2020-04-12 14:09:07 -0500

    Agrega pipes basicos: lowercase, uppercase, titlecase

--- holaMundo/src/app/app.module.ts
+++ holaMundo/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { TitulosComponent } from './titulos/titulos.component';
 import { FormsModule} from '@angular/forms';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { EjemploComponent } from './ejemplo/ejemplo.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 @NgModule({
   declarations: [ //Compoenentes
     AppComponent,
     TitulosComponent,
     DirectivaComponent,
-    EjemploComponent
+    EjemploComponent,
+    PipesComponent
   ],
   imports: [ //Plugins 
     BrowserModule,
