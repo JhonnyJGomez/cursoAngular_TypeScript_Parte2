121b46153d39b60aeb022a28ff8c53b4aeebe48e
Jhonny Gomez <jg.jhonny90@gmail.com>
2020-04-12 16:52:14 -0500

    Agrega pipes percent, currency, date

--- holaMundo/src/app/pipes/pipes.component.ts
+++ holaMundo/src/app/pipes/pipes.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class PipesComponent implements OnInit {
 
   titulo:string = "Soy un titulo"
+  dinero:number = 150020
+  ganacias:number = 0.34
+  fecha = new Date() 
+
   constructor() { }
 
   ngOnInit(): void {
