06f033a3ea6c5be0d7ab350a48a882486f3d7a0b
Jhonny Gomez <jg.jhonny90@gmail.com>
2020-04-13 22:25:33 -0500

    Agrega Router link Active al menu

--- holaMundo/src/app/app-routing.module.ts
+++ holaMundo/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { Pagina404Component } from './pagina404/pagina404.component';
 
 const routes: Routes = [
     {
-        path:'titulos' , component: TitulosComponent
+        path:'' , component: TitulosComponent
     },
     {
         path:'pipes' , component: PipesComponent
